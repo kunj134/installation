e0ce4ad738e439ce7c5a1d3f93a96342ac42f0d2
kunj134 <114722786+kunj134@users.noreply.github.com>
2022-11-02 01:52:46 +0530

    Update combineInstallation.sh

--- combineInstallation.sh
+++ combineInstallation.sh
@@ -65,4 +65,6 @@ sudo apt-get update && sudo apt-get install packer
 #################################################################################################################################
 # Ansible installation
 #################################################################################################################################
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
 sudo apt install ansible -y
