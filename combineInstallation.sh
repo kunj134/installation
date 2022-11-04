cd588a53de732b9f722e29b783ef214fc95b0e53
kunj134 <114722786+kunj134@users.noreply.github.com>
2022-11-04 18:27:20 +0530

    Update combineInstallation.sh

--- combineInstallation.sh
+++ combineInstallation.sh
@@ -68,3 +68,15 @@ sudo apt-get update && sudo apt-get install packer
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible -y
+
+#################################################################################################################################
+# MongoDB Installation
+#################################################################################################################################
+cat <<EOF > mongo_install.sh
+#!/bin/bash
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+apt-key list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update 
+sudo apt install mongodb-org -y
+EOF
